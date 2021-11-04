06937a0ae9b675f4ea1a2ecd3ac092d80844cae5
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-11-04 20:23:46 -0300

    correção da tabela

---
+++ SalesWebMvc/Migrations/20211104222556_correcao-sales-saleRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class correcaosalessaleRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Seller",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Seller",
+                newName: "Nome");
+        }
+    }
+}
