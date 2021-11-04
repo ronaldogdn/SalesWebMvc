d57b67e0388bfc0a57e03ed1de7a8672d71be5ac
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-11-04 18:01:44 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public String Email { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string nome, string email, double baseSalary, Department department)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSale(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+        public void AddSeller(Seller seller)
+        {
+
+        }
+    }
+}
