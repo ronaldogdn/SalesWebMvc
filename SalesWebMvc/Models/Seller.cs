42a7fbe2335991c6b3f4bfc66d9642340872d40b
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-11-08 21:31:52 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,16 +9,27 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage = "{0} size should be between {2} and {1}")]
+        //{0} => é o nome do atributo,neste caso Name
+        //{1} => corresponde ao valor 60
+        //{2} => corresponde ao valor 3
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name ="Birth Date" )]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
